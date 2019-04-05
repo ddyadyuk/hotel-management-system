eb17ee007f46009b212932f00e2d2805ce52aadf
dd <ddyadyuk507@gmail.com>
2019-04-05 18:34:05 +0300

    HMS-5: Create htm-rest-app maven module
    htm-rest-app module was created

---
+++ htm-dao/src/main/java/com/epam/edu/htm/dao/config/DaoConfig.java
@@ -0,0 +1,21 @@
+package com.epam.edu.htm.dao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration@PropertySource("classpath:sql.properties")
+public class DaoConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer appPostgres() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
+                new PropertySourcesPlaceholderConfigurer();
+        propertySourcesPlaceholderConfigurer.setLocations(new ClassPathResource(
+                ""));
+        return propertySourcesPlaceholderConfigurer;
+    }
+
+}
