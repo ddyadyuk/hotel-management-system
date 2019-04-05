dfbccff36804e59876c25bb564aa671f87e72df2
dd <ddyadyuk507@gmail.com>
2019-04-05 18:34:05 +0300

    HMS-5: Create htm-rest-app maven module
    htm-rest-app module was created

--- htm-dao/src/main/java/com/epam/edu/htm/dao/JdbcUserDao.java
+++ htm-dao/src/main/java/com/epam/edu/htm/dao/JdbcUserDao.java
@@ -14,7 +14,6 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
-import java.util.Optional;
 
 @Component
 public class JdbcUserDao implements UserDao {
