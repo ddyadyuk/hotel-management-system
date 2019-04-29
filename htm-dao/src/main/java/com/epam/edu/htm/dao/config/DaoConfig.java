ede87f210934d4e22e6d28eefe5a7546c43e7176
Dmitry Dyadyuk <dzmitry_dziadziuk@epam.com>
2019-04-29 21:05:44 +0300

    HMS-7: Create CRUD address  methods
    CRUD methods were developed and tested

--- htm-dao/src/main/java/com/epam/edu/htm/dao/config/DaoConfig.java
+++ htm-dao/src/main/java/com/epam/edu/htm/dao/config/DaoConfig.java
@@ -1,7 +1,6 @@
 package com.epam.edu.htm.dao.config;
 
 import com.epam.edu.htm.dao.mapper.AddressMapper;
-import com.epam.edu.htm.dao.mapper.UserRowMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -18,11 +17,6 @@ public class DaoConfig {
         propertySourcesPlaceholderConfigurer.setLocations(new ClassPathResource(
                 ""));
         return propertySourcesPlaceholderConfigurer;
-    }
-
-    @Bean
-    public static UserRowMapper userRowMapper() {
-        return new UserRowMapper();
     }
 
     @Bean
