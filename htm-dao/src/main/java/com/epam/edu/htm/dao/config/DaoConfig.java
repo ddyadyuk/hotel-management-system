9c376810dd7c0f230255fd7a214cc5d2ccbf1e03
Dzmitry Dziadziuk <dzmitry_dziadziuk@epam.com>
2019-04-29 19:12:35 +0300

    Hms 6

--- htm-dao/src/main/java/com/epam/edu/htm/dao/config/DaoConfig.java
+++ htm-dao/src/main/java/com/epam/edu/htm/dao/config/DaoConfig.java
@@ -1,6 +1,6 @@
 package com.epam.edu.htm.dao.config;
 
-import com.epam.edu.htm.dao.mapper.AddressMapper;
+import com.epam.edu.htm.dao.mapper.UserRowMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -20,7 +20,8 @@ public class DaoConfig {
     }
 
     @Bean
-    public static AddressMapper addressMapper() {
-        return new AddressMapper();
+    public static UserRowMapper userRowMapper() {
+        return new UserRowMapper();
     }
+
 }
